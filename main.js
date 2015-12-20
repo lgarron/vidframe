e8ce3d9f48ebe7f836a312bfd3a66fab601f56f6
Lucas Garron <code@garron.net>
2015-12-20 11:44:06 -0800

    Initial commit.

---
+++ main.js
@@ -0,0 +1,85 @@
+
+// Some parts and ideas from https://github.com/codepo8/videograbber
+var f;
+
+window.addEventListener("load", function() {
+  var domElement = document.getElementById("main");
+  var feedbackElement = document.getElementById("feedback");
+  var videoElement = document.getElementById("video");
+
+  var fileName = "Video";
+
+  var callback = function(file) {
+    console.log(file);
+    fileName = file.name;
+    videoElement.src = URL.createObjectURL(file);
+  }
+  registerFileDragDrop(domElement, feedbackElement, callback);
+
+  function download(fileFormat) {
+    console.log("Downloading", fileFormat)
+    var canvas = document.getElementById("canvas");
+
+    canvas.width = videoElement.videoWidth;
+    canvas.height = videoElement.videoHeight;
+
+    ctx = canvas.getContext("2d");
+    ctx.drawImage(videoElement, 0, 0, videoElement.videoWidth, videoElement.videoHeight);
+
+    var outputFileNameStem;
+    if (fileName.indexOf(".") === -1) {
+      outputFileNameStem = fileName;
+    } else {
+      var parts = fileName.split(".");
+      parts.splice(parts.length - 1);
+      outputFileNameStem = parts.join(".");
+    }
+
+    var a = document.createElement("a");
+    a.download = outputFileNameStem + " @" + formatTime(videoElement.currentTime) + "." + fileFormat;
+    a.href = canvas.toDataURL("image/" + fileFormat);
+    a.click();
+  }
+
+  document.getElementById("jpeg").addEventListener("click", download.bind(this, "jpeg"));
+  document.getElementById("png").addEventListener("click", download.bind(this, "png"));
+});
+
+
+formatTime = function(time) {
+  // Each entry is [minimum number of digits if not first, separator before, value]
+  var hours   = Math.floor(time / (60 * 60 * 1));
+  var minutes = Math.floor(time / (     60 * 1)) % 60;
+  var seconds = Math.floor(time / (          1)) % 60;
+
+  /**
+   * @param {integer} number
+   * @param {integer} numDigitsAfterPadding
+   */
+  function pad(number, numDigitsAfterPadding)
+  {
+    var output = "" + number;
+    while (output.length < numDigitsAfterPadding) {
+      output = "0" + output;
+    }
+    return output;
+  }
+
+  var secFirstString = "";
+  var secRestString;
+  if (hours > 0) {
+    secRestString = "" + pad(hours, 2) + "-" + pad(minutes, 2) + "-" + pad(seconds, 2);
+  } else if (minutes > 0) {
+    secRestString = "" +                           minutes     + "-" + pad(seconds, 2);
+  } else {
+    secRestString = "" +                                                   seconds    ;
+    if (secRestString[0] === "1") {
+      secFirstString = "1";
+      secRestString = secRestString.substr(1);
+    }
+  }
+
+  var centiseconds = Math.floor(((time * 1000) % 1000) / 10);
+
+  return secFirstString + secRestString + "." + pad(centiseconds, 2);
+}
